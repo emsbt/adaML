00610183676ce5bd4803358c4c2700aefd20b07d
Emerson Tenorio <emersontenorio1@gmail.com>
2025-08-27 22:16:13 -0300

    Refatorando codigo

--- InterfaceDeNotificacoes/src/Main.java
+++ InterfaceDeNotificacoes/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class Principal {
+public class Main {
     public static void main(String[] args) {
         // 1. Criar as diferentes implementações de notificação
         Notificacao email = new Email();
